9bf6b8df5523fdfa14787a8cbd9076f75c29a110
Kaloyan Angelov <75050098+kaloyannbg@users.noreply.github.com>
2021-11-11 02:17:27 +0200

    Update checkFunctions.c

--- functions/checkFunctions.c
+++ functions/checkFunctions.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include "../main.h"
 
-
 int isLetter(char c)
 {
     return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
@@ -14,3 +13,27 @@ int isDigit(char c)
     return (c >= '0' && c <= '9');
 }
 
+int isContinue()
+{
+    char contin = 0;
+    fflush(stdin);
+    getCharFromAdress(&contin);
+    fflush(stdin);
+    if (contin == 'y' || contin == 'Y')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int countDigits(int num)
+{
+    int count = 0;
+    do
+    {
+        num /= 10;
+        ++count;
+
+    } while (num != 0);
+    return num;
+}
